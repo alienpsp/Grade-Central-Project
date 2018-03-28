c2e898292bafba794bc15a8fb315135b305e3e50
alienpsp <ryanchng.ps@gmail.com>
2018-03-28 21:32:09 +0800

    updated code with better functionality.

--- Grade Central v2.py
+++ Grade Central v2.py
@@ -1,6 +1,3 @@
-
-
-
 #Create a student grading system using Python that has the following functionalities:
 #1. Entering the grades of a student
 
@@ -14,29 +11,27 @@
 #- Make sure you use dictionaries and lists for storing student's data.
 #- Us Python functions as much as you can
 
-
-
-'''
-Example of the snapshots:
-
-Welcome to Grade Central 
-
-[1] - Enter Grades
-[2] - Remove Student
-[3] - Student Average Grades
-[4] - Exit
-
-What would you like to do today? (Enter a number)
-'''
-
-
 from statistics import mean as m
+import getpass
 
 
 admins = {'yoid':'yopw', 'admin':'admin', 'Master Wayne':'Batman'}
-studentDict = {'Alex':[99,92,96],
-               'Brandon':[88,89,91],
-               'Cassie':[77,72,75]}
+studentDict = {'Alex':[84,92,96],
+               'Brandon':[88,69,91],
+               'Cassie':[79,72,95]}
+
+def login():
+    login = input('Username: ')
+    if login in admins:
+        pswd = getpass.getpass('Password: ')
+        if admins[login] == pswd:
+            print('Welcome, ', login)
+            while True:
+                main()
+        else:
+            print('Are you sure you are ' + login + ', GPS and IP scanning activated')
+    else:
+        print('Intruder Alert, your picture had been taken and submitted to the hall of failed break-in attempt')
 
 def AddGrades():
     StudentName = input('Student Name: ')
@@ -61,6 +56,16 @@ def avgGrade():
 
         print(eachStudent, 'has an average grade of: ', average)
 
+def AddAdmins():
+    newAdmin = input('Admin Name: ')
+
+    if newAdmin in admins:
+        print('Admin is already registered.')
+    else:
+        newPass = input('Password: ')
+        admins[newAdmin]=newPass
+        print('New admin updated.')
+
 def main():
     print ('''
     Welcome to Grade Central 
@@ -68,7 +73,8 @@ def main():
     [1] - Enter Grades
     [2] - Remove Student
     [3] - Student Average Grades
-    [4] - Exit
+    [4] - Add Admin
+    [5] - Exit
     ''')
 
     action = input('What would you like to do today? (Enter a number)')
@@ -80,21 +86,12 @@ def main():
     elif action == '3':
         avgGrade()
     elif action == '4':
+        AddAdmins()
+    elif action == '5':
         exit()
     elif action == '0':
         print('0')
     else:
         print('command invalid, please try again')
 
-login = input('Username: ')
-passw = input('Password: ')
-
-if login in admins:
-    if admins[login] == passw:
-        print('Welcome, ', login)
-        while True:
-            main()
-    else:
-        print('Are you sure you are ' + login + ', GPS and IP scanning activated')
-else:
-    print('Intruder Alert, your picture had been taken and submitted to the hall of failed break-in attempt')
+login()
